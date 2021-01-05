ed18775c26ab07da44ea7041cc88ec38737714f0
naufalrifai <72070083+naufalrifai@users.noreply.github.com>
2021-01-05 19:34:42 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,78 @@
+//use path module
+const path = require('path');
+//use express module
+const express = require('express');
+//use hbs view engine
+const hbs = require('hbs');
+//use bodyParser middleware
+const bodyParser = require('body-parser');
+//use mysql database
+const mysql = require('mysql');
+const app = express();
+ 
+//konfigurasi koneksi
+const conn = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'arkademy'
+});
+ 
+//connect ke database
+conn.connect((err) =>{
+  if(err) throw err;
+  console.log('Mysql Connected...');
+});
+ 
+//set views file
+app.set('views',path.join(__dirname,'views'));
+//set view engine
+app.set('view engine', 'hbs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+//set folder public sebagai static folder untuk static file
+app.use('/assets',express.static(__dirname + '/public'));
+ 
+//route untuk homepage
+app.get('/',(req, res) => {
+  let sql = "SELECT * FROM produk";
+  let query = conn.query(sql, (err, results) => {
+    if(err) throw err;
+    res.render('product_view',{
+      results: results
+    });
+  });
+});
+ 
+//route untuk insert data
+app.post('/save',(req, res) => {
+  let data = {nama_produk: req.body.nama_produk, keterangan: req.body.keterangan, harga: req.body.harga, jumlah: req.body.jumlah};
+  let sql = "INSERT INTO produk SET ?";
+  let query = conn.query(sql, data,(err, results) => {
+    if(err) throw err;
+    res.redirect('/');
+  });
+});
+ 
+//route untuk update data
+app.post('/update',(req, res) => {
+  let sql = "UPDATE produk SET nama_produk='"+req.body.nama_produk+"', keterangan='"+req.body.keterangan+"', harga='"+req.body.harga+"', jumlah='"+req.body.jumlah+"' WHERE id_produk="+req.body.id;
+  let query = conn.query(sql, (err, results) => {
+    if(err) throw err;
+    res.redirect('/');
+  });
+});
+ 
+//route untuk delete data
+app.post('/delete',(req, res) => {
+  let sql = "DELETE FROM produk WHERE id_produk="+req.body.id_produk+"";
+  let query = conn.query(sql, (err, results) => {
+    if(err) throw err;
+      res.redirect('/');
+  });
+});
+ 
+//server listening
+app.listen(8000, () => {
+  console.log('Server is running at port 8000');
+});
